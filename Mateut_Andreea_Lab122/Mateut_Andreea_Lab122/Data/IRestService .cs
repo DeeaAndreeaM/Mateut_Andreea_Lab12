cb13dd42453cddaae54743f38db467e3b0e25e89
Mateut Andreea Alina <mateutandreea96@gmail.com>
2022-01-16 01:07:54 +0200

    Add project files.

---
+++ Mateut_Andreea_Lab122/Mateut_Andreea_Lab122/Data/IRestService .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Mateut_Andreea_Lab122.Models;
+
+namespace Mateut_Andreea_Lab122.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
---
+++ Mateut_Andreea_Lab122/Mateut_Andreea_Lab122/Data/ShoppingListDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Mateut_Andreea_Lab122.Models;
+
+namespace Mateut_Andreea_Lab122.Data
+{
+    class ShoppingListDatabase
+    {
+        IRestService restService;
+        public ShoppingListDatabase(IRestService service)
+        {
+            restService = service;
+        }
+        public Task<List<ShopList>> GetShopListsAsync()
+        {
+            return restService.RefreshDataAsync();
+        }
+
+        public Task SaveShopListAsync(ShopList item, bool isNewItem = true)
+        {
+            return restService.SaveShopListAsync(item, isNewItem);
+        }
+        public Task DeleteShopListAsync(ShopList item)
+        {
+            return restService.DeleteShopListAsync(item.ID);
+        }
+
+    }
+}
+    
